89881148eec33c3fde3d1f250a36bdf0f6b0ad5c
Enoch Mwangi <enoch.mwangi@outlook.com>
2021-09-07 19:00:11 +0300

    Included consoletables to output the data in a tabular format on the console

--- EmployeePayProject/Program.cs
+++ EmployeePayProject/Program.cs
@@ -35,6 +35,24 @@ namespace EmployeePayProject
 
             return payableTax;
         }
+        static string GenderName(string name)
+        {
+            if(name == "F") 
+                {
+                    name = "Female";
+                }
+            else 
+                {
+                    name = "Male";
+                }
+            return name;
+        }
+        static string EmailVendor(string email) 
+        {
+            char[] separators = { '@', '.' };
+            var emailproviderfinder = email.Split(separators);
+            return emailproviderfinder[2];
+        }
 
         static void Main(string[] args)
         {
@@ -57,14 +75,25 @@ namespace EmployeePayProject
                 //assigning the data to the objects                
                 individuals[i].FirstName = stringedLine[0];
                 individuals[i].LastName = stringedLine[1];
+
                 individuals[i].Gender = stringedLine[2];
+                individuals[i].Gender = GenderName(individuals[i].Gender);
+
                 individuals[i].Email = stringedLine[3];
+                individuals[i].Email = EmailVendor(individuals[i].Email);                
+
                 individuals[i].SalaryPerMonth = Convert.ToDecimal(stringedLine[4]);
                 individuals[i]._Tax = Tax(individuals[i].SalaryPerMonth);
                 i++;
             }
 
-            var table = new ConsoleTable("Full Name", "Gender", "Email Provider", "GrossPay", "PAYE")
+            var table = new ConsoleTable("Full Name", "Gender", "Email Provider", "GrossPay", "PAYE");
+            for (int z = 0; z < details.Length; z++)
+            {
+                table.AddRow(individuals[z].FirstName + " " + individuals[z].LastName, individuals[z].Gender, individuals[z].Email, Decimal.Round(individuals[z].SalaryPerMonth, 2), Decimal.Round(individuals[z]._Tax, 2));
+            }
+            table.Write();
+        
         }
     }
 }
